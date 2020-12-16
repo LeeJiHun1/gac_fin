075aee129daa0d756566b53b81dc0f894b07b0e7
JinHojun <silvergold9@naver.com>
2020-12-17 00:59:42 +0900

    썸네일에서 원화표시 추가

--- gachon_flea/views.py
+++ gachon_flea/views.py
@@ -228,6 +228,7 @@ class SearchFormView(FormView):
 class mywallet(ListView):
     model = Product
     template_name = 'gachon_flea/mypage/mypage_wallet.html'
+    
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
